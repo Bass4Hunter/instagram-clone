688128f108c641e150fb31042c1cba8f78988f46
Gabriel Gonza <gabrielito.xvr@gmail.com>
2023-03-30 12:17:25 -0500

    wip: post card

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import Header from "../elements/Header";
 import Footer from "../elements/Footer";
 import MainFeed from "../views/MainFeed";
+import "@fontsource/noto-sans-jp";
 
 export default function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen max-h-screen bg-zinc-200">
-      <div className="flex flex-col min-h-screen max-h-screen bg-amber-300">
-        <Header />
-        <MainFeed />
-        <Footer />
+    <>
+      <div
+        className="flex justify-center items-center min-h-screen max-h-screen bg-zinc-200"
+        style={{ fontFamily: "Noto Sans JP" }}
+      >
+        <div className="flex flex-col min-h-screen max-h-screen bg-amber-300">
+          <Header />
+          <MainFeed />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
